bc77c543e88dbb6f185a138cd21105158f302422
Woohyuck-Jeoong <jwhyukk10@gmail.com>
2025-03-19 08:58:41 +0900

    TCP/IP Test

---
+++ EthernetCommunication/TCP_IP_Test/tcp_client.py
@@ -0,0 +1,29 @@
+# 데이터 송신 코드 - 라즈베리파이 4
+import socket, time
+
+# 윈도우와 IP, 포트 설정
+SERVER_IP = "192.168.1.100"     # 윈도우 노트북의 실제 IP 주소
+PORT = 5000                     # 서버에서 설정한 포트
+
+# TCP 소켓 생성 및 연결
+client_socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+client_socket.connect((SERVER_IP, PORT))
+
+print(f"[CLIENT] {SERVER_IP}: {PORT} 서버에 연결됨.")
+
+count = 1
+try:
+    while True:
+        msg = str(f"$000-938-0290+0500,+0192-0258+{count},1,0").encode()        # 숫자를 문자열로 변환 후 바이트로 인코딩
+        client_socket.sendall(msg)                                              # 데이터 전송
+        print(f"[CLIENT] 전송: {str(msg)}")
+        count += 1
+        time.sleep(1)       # 1초 대기
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("\n[CLIENT] 종료")
+finally:
+    client_socket.close()
---
+++ EthernetCommunication/TCP_IP_Test/tcp_server.py
@@ -0,0 +1,31 @@
+# 수신 코드 - 윈도우
+import socket
+
+# 서버 설정
+HOST = "0.0.0.0"        # 모든 네트워크 인터페이스에서 연결 허용
+PORT = 5000             # 포트 번호
+
+# TCP 소켓 생성
+server_socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)             # 하나의 클라이언트만 연결 허용
+
+print(f"[SERVER] {PORT} 포트에서 데이터 수신 대기 중...")
+
+conn, addr = server_socket.accept()
+print(f"[SERVER] {addr}에서 연결됨.")
+
+try:
+    while True:
+        data = conn.recv(1024).decode()     # 데이터 수신
+        if not data:
+            break
+        print(f"[SERVER] 수신: {data}")
+except KeyboardInterrupt:
+    print("\n[SERVER] 종료")
+finally:
+    conn.close()
+    server_socket.close()
